ce0b8a4f414bf8d246e4d2861d150cd6bbb24e55
anis MEKBAL <anis.mekbal@etudiant.univ-brest.fr>
2025-01-03 19:32:22 +0100

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "fr.mekbal_dev.recipe_app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "fr.mekbal_dev.recipe_app"
+        minSdk = 28
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //navigation componenet
+    val nav_version = "2.8.4"
+    //implementation("androidx.navigation:navigation-fragment:$nav_version")
+    //implementation("androidx.navigation:navigation-ui:$nav_version")
+
+    implementation ("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation ("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+    //size screen
+    implementation("com.intuit.sdp:sdp-android:1.0.6")
+    implementation("com.intuit.ssp:ssp-android:1.0.6")
+
+    implementation("network.chaintech:sdp-ssp-compose-multiplatform:1.0.4")
+
+    //loaded animation
+    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.17")
+
+
+
+}
